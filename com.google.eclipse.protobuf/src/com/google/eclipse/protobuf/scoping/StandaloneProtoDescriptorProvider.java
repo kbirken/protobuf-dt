88d926ba87d02e2cb932e4d6a12196dd14b1f09b
Klaus Birken <klaus.birken@gmail.com>
2016-06-23 13:39:10 +0200

    Removed debug output.

--- com.google.eclipse.protobuf/src/com/google/eclipse/protobuf/scoping/StandaloneProtoDescriptorProvider.java
+++ com.google.eclipse.protobuf/src/com/google/eclipse/protobuf/scoping/StandaloneProtoDescriptorProvider.java
@@ -20,16 +20,13 @@ public class StandaloneProtoDescriptorProvider extends ProtoDescriptorProvider {
 	protected ProtoDescriptorInfo defaultDescriptorInfo() {
 		URL url = getClass().getClassLoader().getResource("descriptor.proto");
 		String urlString = url.toString();
-		System.out.println("StandaloneProtoDescriptorProvider - URL      : " + urlString);
+//		System.out.println("StandaloneProtoDescriptorProvider - URL      : " + urlString);
 		
 		URI location = URI.createURI(urlString);
-		if (! urlString.equals(location.toString()))
-			System.out.println("StandaloneProtoDescriptorProvider - URI      : " + location.toString());
+//		if (! urlString.equals(location.toString()))
+//			System.out.println("StandaloneProtoDescriptorProvider - URI      : " + location.toString());
 
-		String extForm = url.toExternalForm();
-		if (! urlString.equals(extForm))
-			System.out.println("StandaloneProtoDescriptorProvider - ext form : " + extForm);
-
+//		URI uri2 = URI.createURI("descriptor.proto");
 		return new ProtoDescriptorInfo(ProtoDescriptorProvider.PROTO_DESCRIPTOR_URI, location);
 	}
 
